d9100ef792668489090cac853886d23cfa4d0b48
soiqualang <soiqualang.chentreu@gmail.com>
2019-07-16 09:58:31 +0200

    u

--- example3.js
+++ example3.js
@@ -13,14 +13,33 @@ var map = L.map('map', {
 //http://girs.vn:8081/geoserver15/test/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities
 
 var atlas_vietnamWMS = "http://dev.dothanhlong.org:8080/geoserver/atlas_vietnam/wms";
+var testWMS = "https://ogcie.iblsoft.com/metocean/wms";
 
-var testWMS = "https://ogcie.iblsoft.com/metocean/wms"
 var baseLayer=L.tileLayer.wms(testWMS, {
     layers: 'foreground-lines',
     format: 'image/png',
     transparent: true,
     crs: L.CRS.EPSG4326
 });
+var vn_ub=L.tileLayer.wms(atlas_vietnamWMS, {
+    layers: 'atlas_vietnam:1vn_ub',
+    format: 'image/png',
+    transparent: true,
+    crs: L.CRS.EPSG4326
+});
+var vn_tinh_hsts=L.tileLayer.wms(atlas_vietnamWMS, {
+    layers: 'atlas_vietnam:1vn_tinh_hsts',
+    format: 'image/png',
+    transparent: true,
+    crs: L.CRS.EPSG4326
+});
+var vn_tinh=L.tileLayer.wms(atlas_vietnamWMS, {
+    layers: 'atlas_vietnam:vn_tinh',
+    format: 'image/png',
+    transparent: true,
+    crs: L.CRS.EPSG4326
+});
+
 
 var testLayer = L.tileLayer.wms(atlas_vietnamWMS, {
         layers: 'timeline_georeferencer',
@@ -30,7 +49,9 @@ var testLayer = L.tileLayer.wms(atlas_vietnamWMS, {
     time: '0001-01-01T00:00:00'
     //opacity: 0.3,
     //attribution: 'hahahaha'
-}).addTo(map);
+});
+
+
 var proxy = 'proxy.php';
 var testTimeLayer = L.timeDimension.layer.wms(testLayer, {
     proxy: proxy,
@@ -39,6 +60,20 @@ var testTimeLayer = L.timeDimension.layer.wms(testLayer, {
 testTimeLayer.addTo(map);
 
 baseLayer.addTo(map);
+
+var baseMaps = {
+    "Lớp nền": baseLayer
+};
+
+var overlayMaps = {
+    "Lãnh thổ Việt Nam": testTimeLayer,
+    "Tỉnh thành":vn_tinh_hsts,
+    "Ranh giớiTỉnh":vn_tinh,
+    "Ủy ban":vn_ub
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 /*
 var testLegend = L.control({
     position: 'topright'
